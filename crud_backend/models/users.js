700f051fcf8be63735073654a6ce9a693ae74cef
pavneetkaur27 <pavneetkaur.jag@gmail.com>
2020-05-01 23:11:21 +0530

    models

--- crud_backend/models/users.js
+++ crud_backend/models/users.js
@@ -1,14 +1,14 @@
 const mongoose 	    = require('mongoose');
 const Schema 	    = mongoose.Schema;
 
-const model_name    = 'users';
+const model_name    = 'user';
 
 const model_schema  = new mongoose.Schema(
 	{
         u_name 		: { type : String   , required : true },
         e_mail 		: { type : String   , required : true},
         stts 		: { type : String   , required : true },
-        rol_nm 		: { type : String   , required : true  },
+        role 		: { type : String   , required : true  },
 		act         : { type : Boolean  ,  required : true },
 	},
 	{
