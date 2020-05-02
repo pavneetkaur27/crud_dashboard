ecdc7ed8229355adf746fb37be10bbf961045324
pavneetkaur27 <pavneetkaur.jag@gmail.com>
2020-05-02 22:44:21 +0530

    frontend build

---
+++ crud_frontend/build/precache-manifest.21e3527a6455993bff9dec51306e846a.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80334671b855872a4d1b41e0993eef49",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2b822333cbfe72788de",
+    "url": "/static/css/main.869c215d.chunk.css"
+  },
+  {
+    "revision": "3862ad76bfc3a93661e1",
+    "url": "/static/js/2.48890446.chunk.js"
+  },
+  {
+    "revision": "d2b822333cbfe72788de",
+    "url": "/static/js/main.b02180dc.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2267f2352ed2a372aacb5b212d150e14",
+    "url": "/static/media/Logo.2267f235.svg"
+  },
+  {
+    "revision": "d966336d6962ee13ee7da522423e7118",
+    "url": "/static/media/ico_active.d966336d.svg"
+  },
+  {
+    "revision": "0a515957da952a5863f02d5af73c2e0e",
+    "url": "/static/media/ico_add.0a515957.svg"
+  },
+  {
+    "revision": "9e6b13d904a23dc09b436bba0953d0a4",
+    "url": "/static/media/ico_dashboard.9e6b13d9.svg"
+  },
+  {
+    "revision": "ab2990b99a8fcc025c93f252277bc137",
+    "url": "/static/media/ico_downarrow.ab2990b9.svg"
+  },
+  {
+    "revision": "b18b2f7ec38b55ce05559465fd6be233",
+    "url": "/static/media/ico_edit.b18b2f7e.svg"
+  },
+  {
+    "revision": "92eb4aec689f9e0da200b724add875f7",
+    "url": "/static/media/ico_inactive.92eb4aec.svg"
+  },
+  {
+    "revision": "5c692bb7c4a4563de23100a5a14151c7",
+    "url": "/static/media/ico_notification.5c692bb7.svg"
+  },
+  {
+    "revision": "28dc8c25fb84bf3fd0beb758166ca3fc",
+    "url": "/static/media/ico_pending.28dc8c25.svg"
+  },
+  {
+    "revision": "75e288e4fb3ae72ff130ceb8c6aadc7f",
+    "url": "/static/media/ico_search.75e288e4.svg"
+  },
+  {
+    "revision": "02eb3ddc431e30390ea97d2a4e6ae56f",
+    "url": "/static/media/ico_sessionmanager.02eb3ddc.svg"
+  },
+  {
+    "revision": "bbd932d18103ed632958e2afbb8f8977",
+    "url": "/static/media/ico_sorting.bbd932d1.svg"
+  },
+  {
+    "revision": "e64175592fa90b538bf891830c1d7878",
+    "url": "/static/media/ico_user.e6417559.svg"
+  },
+  {
+    "revision": "d7e612e0c49b4927ae4549d9b9177f8d",
+    "url": "/static/media/ico_users.d7e612e0.svg"
+  },
+  {
+    "revision": "86c4c0922c36f634897417df21a73bbb",
+    "url": "/static/media/ico_users_white.86c4c092.svg"
+  },
+  {
+    "revision": "940e1eef6ecc343fc47455f2ade8c9e1",
+    "url": "/static/media/icon_delete.940e1eef.jpg"
+  },
+  {
+    "revision": "707852f13145a7f5606b4a83505faf6f",
+    "url": "/static/media/loader.707852f1.svg"
+  }
+]);
--- crud_frontend/scripts/deploy.js
+++ crud_frontend/scripts/deploy.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 9000;
+const port = 7070;
 
 app.use(express.static('./build'));
 
 app.get('/', function (req, res) {
+  console.log("frontend started at port :"+ port);
   res.sendFile(path.join(__dirname, '../build', 'index.html'));
 });
 
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, '../build', 'index.html'));
+  console.log("frontend started at port :"+ port);
+  res.sendFile(path.join(__dirname, '../build', 'index.html'));
 });
 
 app.listen(port);
