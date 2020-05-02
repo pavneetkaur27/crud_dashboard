e92cf36d7b61cd038ab6affa8109a080f9bbe960
pavneetkaur27 <pavneetkaur.jag@gmail.com>
2020-05-03 02:27:38 +0530

    errors updated

--- crud_backend/helper/Errors.js
+++ crud_backend/helper/Errors.js
@@ -1,8 +1,9 @@
 "use strict";
 var errors = {
-	"invalid_parameters" 										: [100, "Invalid Parameters."],
+	"invalid_parameters" 										: [100, "Invalid Parameters"],
 	"server_error"		 										: [500, "Something went wrong"],
-	"no_server_response"										: [2300,"No response from server."],
+	"no_server_response"										: [2300,"No response from server"],
+	"invalid_email"												: [101, "Invalid Email Id"]
 };
 
 module.exports = errors; 
--- crud_backend/helper/HttpResponse.js
+++ crud_backend/helper/HttpResponse.js
@@ -2,32 +2,32 @@ const constants = require('./Constants');
 const Errors = require('./Errors');
 const isUndefined = require('bvalid').isUndefined;
 
-const sendError = (_r, e, e_idx, s_c) => {
-	console.trace(e);
+const sendError = (res, err, err_index, status_code) => {
+	console.trace(err);
 	var errMsg;
 	var errCode;
-	s_c = (isUndefined(s_c) || isUndefined(Errors[e_idx]))
-		? constants.HTTP_STATUS.SERVER_ERROR : s_c;
+	status_code = (isUndefined(status_code) || isUndefined(Errors[err_index]))
+		? constants.HTTP_STATUS.SERVER_ERROR : status_code;
 
-	if(isUndefined(Errors[e_idx])) {
+	if(isUndefined(Errors[err_index])) {
 		errCode = errorCodes["server_error"][0]
 		errMsg = errorCodes["server_error"][1];
 	} else {
-		errCode = Errors[e_idx][0]
-		errMsg = Errors[e_idx][1];
+		errCode = Errors[err_index][0]
+		errMsg = Errors[err_index][1];
 	}
 
-	return _r.status(s_c).send({
+	return res.status(status_code).send({
 		code: errCode,
 		err: errMsg,
-		suc: false
+		success: false
 	});
 }
 
-const sendSuccess = (_r,_d)=>{
-	return _r.status(constants.HTTP_STATUS.OK).send({
-		data: _d,
-		suc: true
+const sendSuccess = (res,data)=>{
+	return res.status(constants.HTTP_STATUS.OK).send({
+		data: data,
+		success: true
 	})
 }
 
